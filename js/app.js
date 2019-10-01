07048038d7f629db84b537c6620e405e5ec75d2e
VanessaLiu-forever18 <52699764+VanessaLiu-forever18@users.noreply.github.com>
2019-10-02 02:05:55 +0930

    Enable users to add item and delete item in the list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+var todos = [
+	{ text: 'Eat', completed: true },
+	{ text: 'Sleep', completed: false },
+	{ text: 'Code', completed: true },
+]
+
+	new Vue({
+		el:'#app',
+		data:{
+			title:'Todo List',
+			todos: todos,
+			text:''
+		},
+		methods:{
+			addItem: function(){
+				if (this.text.trim().length === 0){
+					return
+				}
+				this.todos.push({
+					text: this.text,
+					completed: false
+				})
+				this.text=''
+			},
+			destroy: function(text){
+				var _that = this
+				var todoIndex
+				this.todos.find(function (todo,index) {
+					if (todo.text === text){
+						todoIndex = index
+					}
+				})
+				this.todos.splice(todoIndex,1)
+			}
+		}
+	})
 
 })(window);
